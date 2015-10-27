cc8179ee5357ca01587f13df3d080eafffe212bc
danleyb2 <ndieksman@gmail.com>
2015-10-27 22:32:01 +0300

    syntax correction,missing comma

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=[
         'Django==1.8.4',
         'requests==2.8.1',
-        'python-instagram'
+        'python-instagram',
 		'authomatic',
     ],
     dependency_links=[
