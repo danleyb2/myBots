3bc4c345f4b2999d6aece732f4b173fbac35b7c8
danleyb2 <ndieksman@gmail.com>
2015-10-27 16:02:31 +0300

    get likes and comments data from posts

--- wsgi/myproject/Ibot/views.py
+++ wsgi/myproject/Ibot/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 import requests
 import json
 from .config import CONFIG,ACCESS_TOKEN
+from .ibot import loaded
 
 # Create your views here.
 
@@ -26,17 +27,21 @@ class Ibot:
 def index(request):
     bot = Ibot(ACCESS_TOKEN)
     # feed=bot.get_req(Ibot.API_URL+'self/feed').text
-
+    '''
     feed = requests.get(
         'https://api.instagram.com/v1/users/self/feed?access_token='+ACCESS_TOKEN).text
     data = json.loads(feed)
+    '''
+    data=loaded
 
     users=[]
     for i in data['data']:
         d=i['user']
         d['mid']=i['id']
         d['likes_count']=i['likes']['count']
+        d['likes']=i['likes']['data']
         d['comments_count']=i['comments']['count']
+        d['comments']=i['comments']['data']
         d['link']=i['images']['low_resolution']['url']
         users.append(d)
 
@@ -47,6 +52,11 @@ def index(request):
 
 
 def like(request,media_id):
+    furl = "https://api.instagram.com/v1/media/"+media_id+"/likes?access_token=" + ACCESS_TOKEN
+    feed=requests.post(furl)
+    return HttpResponseRedirect(reverse('ibot/index'))
+
+def comment(request,media_id):
     furl = "https://api.instagram.com/v1/media/"+media_id+"/likes?access_token=" + ACCESS_TOKEN
     feed=requests.post(furl)
     return HttpResponseRedirect(reverse('ibot/index'))
